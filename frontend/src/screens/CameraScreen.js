2eab0444f81868624a1e135d0a87e6ebfdbcea5d
SJ-Kwak <seojin7@ewhain.net>
2022-07-04 17:05:49 +0900

    [fix] Change navigation bar style

---
+++ frontend/src/screens/CameraScreen.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { images } from '../images';
+
+const Camera = () => {
+
+    return (
+        <View>
+            <Text>Camera</Text>
+        </View>
+    )
+}
+
+
+
+export default Camera;
