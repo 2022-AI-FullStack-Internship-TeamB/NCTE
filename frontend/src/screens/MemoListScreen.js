8644c113771da50e352fa33d950eb0278e91d395
SJ-Kwak <seojin7@ewhain.net>
2022-07-11 14:46:19 +0900

    [fix] Fix memolist navigation

--- frontend/src/screens/MemoListScreen.js
+++ frontend/src/screens/MemoListScreen.js
@@ -6,24 +6,23 @@ import IconButton from '../components/IconButton';
 
 const MemoListScreen = ({ navigation }) => {
 
-  const [writeMode, setWriteMode] = useState(false); 
-  const [txt, setTxt] = useState(''); 
-
   var today = new Date();
   var year = today.getFullYear();
   var month = ('0' + (today.getMonth() + 1)).slice(-2);
   var day = ('0' + today.getDate()).slice(-2);
-  //console.log(today)  
   var dateString = year + '년 ' + month  + '월 ' + day + '일';
 
-  const orimemo = [];
+  const orimemo = [
+    {
+      id: '1',
+      memo: '실리콘밸리 AI 인턴십'
+    }
+  ];
 
-  const [memos, setMemos] = useState(orimemo); 
-  const [Idx, setIdx] = useState(1); 
+  const [memos, setMemos] = useState(orimemo);
 
   const add = () => {
-    navigation.navigate('Camera');
-    //console.log('add');
+    navigation.navigate('CameraStack');
   }
 
   const back = () => {
@@ -34,14 +33,7 @@ const MemoListScreen = ({ navigation }) => {
     navigation.navigate('Note');
   }
 
-  const addMemo = () =>{
-    let a = {Id:Idx, memo:txt};
-    setMemos(prev=>[...prev,a]); 
-    setWriteMode(false); 
-    setIdx(prev=>prev+1); 
-  }
-
-  const renderMemo = ({item}) => {
+  const renderMemo = ({ item }) => {
     return(
       <TouchableOpacity style = {boxStyles.memo} onPress = {_onPress}>
         <View style ={textStyles.InBox}>
@@ -53,44 +45,19 @@ const MemoListScreen = ({ navigation }) => {
             {item.memo}
           </Text>
         </View>
+
         <View>
-        <Text 
-          style = {{ marginLeft: 35, }}
-        >
-          {dateString}
-        </Text>
+          <Text 
+            style = {{ marginLeft: 35, }}
+          >
+            {dateString}
+          </Text>
         </View>
       </TouchableOpacity>
     );
   }
 
-  if(writeMode){
-    return (
-      <View style={boxStyles.top}>        
-        <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-          <TouchableOpacity style={{padding:15, }} onPress={()=>setWriteMode(false)}>
-            <Text style={{fontSize:18, }} >취소</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={{padding:15, }}     onPress={()=>addMemo()} >
-            <Text style={{fontSize:18, }}>저장</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={{flex:1, backgroundColor:'#fff', }}>
-        <TextInput
-            style={{  backgroundColor: '#eee',flex:1, padding:10,  }}
-            onChangeText={text => setTxt(text)}
-            multiline 
-            
-          />
-        </View>
-      </View>
-    );
-  }
-
   return (
-
     <View>
         <View style={boxStyles.top}>
           <View style = {viewStyles.row}>
@@ -109,7 +76,7 @@ const MemoListScreen = ({ navigation }) => {
               marginLeft = {240}
               marginTop = {50}
             />
-          </View> 
+          </View>
         </View>
         
         <View>
