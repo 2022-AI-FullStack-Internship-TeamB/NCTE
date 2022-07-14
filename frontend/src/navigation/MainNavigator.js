ca4b79b8eabb349031630b8e7af1ec5c15831c38
SJ-Kwak <seojin7@ewhain.net>
2022-07-14 12:07:49 +0900

    [feat] Connect mypage api

--- frontend/src/navigation/MainNavigator.js
+++ frontend/src/navigation/MainNavigator.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { images } from '../images';
 import { theme } from '../theme';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import TabNavigator from './TabNavigator';
 import { SignInStack } from './StackNavigator';
@@ -11,15 +12,30 @@ import { SignInStack } from './StackNavigator';
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
+    
+    const [isLogin, setIsLogin] = useState(false);
+    useEffect(() => {
+        //AsyncStorage.getItem('isLogin');
+        if (AsyncStorage.getItem('isLogin') === true){
+            setIsLogin(true);
+        }
+    })
+
     return (
         <NavigationContainer
             independent = {true}
         >
             <Stack.Navigator
                 screenOptions = {{ headerShown: false }}
+                initialRouteName = {SignInStack}
             >
-                <Stack.Screen name = 'SignInStack' component = {SignInStack} />
-                <Stack.Screen name = 'TabNavigator' component = {TabNavigator} />
+            {
+                isLogin ? (
+                    <Stack.Screen name = 'TabNavigator' component = {TabNavigator} />    
+                ) : (
+                    <Stack.Screen name = 'SignInStack' component = {SignInStack} />
+                )
+            }
             </Stack.Navigator>
         </NavigationContainer>
     )
