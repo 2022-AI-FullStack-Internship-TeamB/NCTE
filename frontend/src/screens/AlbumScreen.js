11e078cc9718c11ab559a197a203fabd0e3ccc20
SJ-Kwak <seojin7@ewhain.net>
2022-07-18 14:03:31 +0900

    [feat] Connect api for notes with category

--- frontend/src/screens/AlbumScreen.js
+++ frontend/src/screens/AlbumScreen.js
@@ -1,17 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import { textStyles, viewStyles, boxStyles, iconStyles } from '../styles';
 import { images } from '../images';
 import { theme } from '../theme';
 import AlbumButton from '../components/AlbumButton';
 import IconButton from '../components/IconButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Album = ({ navigation }) => {
+
+    const [categoryName, setCategoryName] = useState('');
+    const [userId, setUserId] = useState('');
+
+    const getId = async () => {
+        try {
+            const user_id = await AsyncStorage.getItem('user_id');
+            setUserId(user_id);
+            console.log('getting id successed in album screen', userId);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+    
+    useEffect(() => {
+        getId();
+        //console.log('userid: ', userId);
+    }, []);
 
     const _onPress = () => {
         console.log('pressed');
     }
+    
+    const onAllPressed = () => {
+        navigation.navigate('List', {
+            categoryName: 'NCTE',
+            userId: userId
+        });
+    }
 
+    const onDiaryPressed = () => {
+        navigation.navigate('List', {
+            categoryName: 'Diary',
+            userId: userId
+        });
+    }
+
+    const onTodoPressed = () => {
+        navigation.navigate('List', {
+            categoryName: 'Todo',
+            userId: userId
+        });
+    }
+
+    const onStudyPressed = () => {
+        navigation.navigate('List', {
+            categoryName: 'Study',
+            userId: userId
+        });
+    }
+    
     const onAlbumPressed = () => {
         navigation.navigate('List');
     }
@@ -30,24 +77,24 @@ const Album = ({ navigation }) => {
             <View style = {viewStyles.container}>
                 <View style = {viewStyles.row}>
                     <AlbumButton
-                        onPress = {onAlbumPressed}
+                        onPress = {onAllPressed}
                         text = 'All'
                         color = {theme.light_blue}
                     />
                     <AlbumButton
-                        onPress = {onAlbumPressed}
+                        onPress = {onDiaryPressed}
                         text = 'Diary'
                         color = {theme.light_pink}
                     />
                 </View>
                 <View style = {viewStyles.row}>
                     <AlbumButton
-                        onPress = {onAlbumPressed}
+                        onPress = {onTodoPressed}
                         text = 'Todo'
                         color = {theme.magenta_pink}
                     />
                     <AlbumButton
-                        onPress = {onAlbumPressed}
+                        onPress = {onStudyPressed}
                         text = 'Study'
                         color = {theme.light_blue}
                     />
