595fbc6097a36c381e0395d13b7025cb6c645b38
SJ-Kwak <seojin7@ewhain.net>
2022-07-19 14:25:41 +0900

    [fix] Fix api url for each platform

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
+import { Platform } from 'react-native';
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://10.0.2.2:8000/api',
+    baseURL: 
+        Platform.OS === 'ios' ? 
+            'http://127.0.0.1:8000/api' : 'http://10.0.2.2:8000/api',
     headers: {
         'Content-Type' : 'application/json',
     },
